defcd2e5033ad27573b5fb74ed5cffe595415a39
Brandon <brhaskett@yahoo.com>
2015-10-17 21:30:13 -0400

    setting up for project two

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,244 +1,254 @@
-#include <iostream>
 #include <GL/glew.h>
 #include <GL/freeglut.h>
-#include "vgl.h"
-#include "vmath.h"
-#include "LoadShaders.h"
+#include <iostream>
+#include <cstdlib>
+#include <fstream>
+#include <string>
+#include <cmath>
+#include <algorithm>
 using namespace std;
 
-float *readOBJFile(string filename, int &nbrTriangles);
 
 /*
-* This is an example of drawing an Icosahedron that we worked on in class.
-* The icosahedron is read from data in the application and not from the
-* file (it was copied from the file).  It is really just a revision of
-* the element draw example we worked on earlier.
+* This program is to read in elevation data from a NASA 1 degree file.
 *
-* Author:  Jerry Heuring
-* Date:    October 6, 2014
+* Author :  Jerry Heuring
+* Date:  November 7, 2013
 *
-* Bugs:
+* Warning:  Code is not finished.  It will read in the file.  You will
+*           need to alter it or just tear out the stuff to read the file
+*           if you want to use it.
 *
+*/
+
+const int NBR_LINES = 3601;
+const int NBR_TRIANGLES_PER_QUAD = 2;
+const int NBR_FLOATS_PER_POINT = 3;
+const int NBR_POINTS_PER_TRIANGLE = 3;
+
+static float *fileData;
+
+void readLineFromFile(istream& inFile, float *line){
+	unsigned char inputline[NBR_LINES * 2];
+
+	inFile.read((char *)inputline, NBR_LINES * 2);
+	for (int i = 0, j = 0; j < NBR_LINES; i += 2, j++) {
+		line[j] = float((unsigned int)((inputline[i] << 8) | inputline[i + 1]));
+	}
+	return;
+
+}
+
+/*
 *
 * Revisions:
-*	10/7/2014 -- added a command line option to choose between using
-*                glDrawElements and glDrawArray.
-*
-*
+*    September 22, 2016 -- Updated readFileData to calculate the extent of the
+*                          data in the x, y, and z directions as well.
 */
-#define BUFFER_OFFSET(x)  ((const void*) (x))
-
-/*
-* Arrays to store the indices/names of the Vertex Array Objects and
-* Buffers.  Rather than using the books enum approach I've just
-* gone out and made a bunch of them and will use them as needed.
-*
-* Not the best choice I'm sure.
-*/
-
-GLuint vertexBuffers[10], arrayBuffers[10], elementBuffers[10];
-
-/*
-* Global variables
-*   The location for the transformation and the current rotation
-*   angle are set up as globals since multiple methods need to
-*   access them.
-*/
-
-GLuint transformLocation, rotationLocation;
-float  rotationAngle;
-bool   elements;
-int    nbrTriangles;
-
-/*
-* Timer routine -- when called it increments the angle by 1 degree,
-* tells Glut to redraw the screen, and sets a timer for 1/30 of a
-* second later.
-*/
-void timer(int value) {
-	rotationAngle += 1.0f;
-	if (rotationAngle > 360) {
-		rotationAngle = rotationAngle - 360;
+float *readFileData(string filename){
+	/* extent holds the minimum x, y, z and the maximum x, y, z */
+	float extent[6];
+	float *triangles = NULL;
+	float *lastLine = NULL;
+	float *currentLine = NULL;
+	ifstream inFile;
+	int linesRead, currentTriangle;
+	/*
+	* open the file for input.
+	*/
+	extent[0] = extent[2] = 0.0;
+	extent[3] = extent[5] = 3600.0 * 30.0;
+	extent[1] = 65535.0;
+	extent[4] = -65536.0;
+	inFile.open(filename.c_str());
+	if (inFile.fail()) {
+		cerr << "Unable to open file \"" << filename << "\" for input " << endl;
+		return triangles;
 	}
-	glutPostRedisplay();
-	glutTimerFunc(1000 / 30, timer, 1);
+	/*
+	* Create an array that is big enough to hold the triangles that we are going to create.
+	*
+	* Have used lots of defines here so that I can change between file types more
+	* easily.
+	*/
+	triangles = new float[(NBR_LINES - 1)* (NBR_LINES - 1) * NBR_TRIANGLES_PER_QUAD *
+		NBR_POINTS_PER_TRIANGLE * NBR_FLOATS_PER_POINT];
+	/*
+	* Hold two lines at a time.  the lastLine and the currentLine.  The
+	* application will swap between them as necessary.
+	*/
+	lastLine = new float[NBR_LINES];
+	currentLine = new float[NBR_LINES];
+	/*
+	* Check for a memory allocation error -- it can happen.
+	*/
+	if (triangles == NULL || lastLine == NULL || currentLine == NULL) {
+		if (triangles != NULL) {
+			delete[] triangles;
+			triangles = NULL;
+		}
+		if (lastLine != NULL) {
+			delete[] lastLine;
+		}
+		if (currentLine != NULL) {
+			delete[] currentLine;
+		}
+		return triangles;
+	}
+	/*
+	* Read in the lines from the file and connect triangles together...
+	*/
+	readLineFromFile(inFile, lastLine);
+	currentTriangle = 0;
+	linesRead = 1;
+	int thirtyLinesRead = 30, thirtyLinesRead_minus_1 = 0;
+	for (linesRead = 1; linesRead < NBR_LINES - 1; linesRead++) {
+		readLineFromFile(inFile, currentLine);
+		/*
+		*Generate Triangles  -- two triangles per quad.
+		*/
+		int thirtyColumn = 0, thirtyColumn_plus_1 = 30;
+		for (int column = 0; column < NBR_LINES; column++) {
+			triangles[currentTriangle] = triangles[currentTriangle + 3] = triangles[currentTriangle + 15] = float(thirtyColumn);
+			triangles[currentTriangle + 6] = triangles[currentTriangle + 9] = triangles[currentTriangle + 12] = float(thirtyColumn_plus_1);
+			triangles[currentTriangle + 1] = lastLine[column];
+			triangles[currentTriangle + 4] = triangles[currentTriangle + 16] = currentLine[column];
+			triangles[currentTriangle + 7] = triangles[currentTriangle + 10] = lastLine[column + 1];
+			triangles[currentTriangle + 13] = currentLine[column + 1];
+			triangles[currentTriangle + 2] = triangles[currentTriangle + 8] = triangles[currentTriangle + 11] = float(thirtyLinesRead_minus_1);
+			triangles[currentTriangle + 5] = triangles[currentTriangle + 14] = triangles[currentTriangle + 17] = float(thirtyLinesRead);
+			currentTriangle += 18;
+			extent[1] = min(extent[1], lastLine[column]);
+			extent[4] = max(extent[4], lastLine[column]);
+			thirtyColumn = thirtyColumn_plus_1;
+			thirtyColumn_plus_1 += 30;
+		}
+
+
+		/*
+		* Swap arrays -- do not want to reallocate them.
+		*/
+		float *temp;
+		temp = lastLine;
+		lastLine = currentLine;
+		currentLine = temp;
+		thirtyLinesRead_minus_1 = thirtyLinesRead;
+		thirtyLinesRead += 30;
+	}
+	delete[] lastLine;
+	delete[] currentLine;
+	return triangles;
 }
 
 /*
-* Init will set up our array objects, buffers, and shaders.
-* The shaders are hard coded right now.  I'm really tempted to
-* build something into the main program to handle a command line
-* argument for the shaders and pass the result into here to make
-* it easy to change shaders.
+* Initialize still needs to load shaders and build program.
 */
-void init() {
-
-	/*
-	* our data is static so that it doesn't disappear with the stack.  Also, the
-	* data for the Icosahedron is being stored in indexed polygon form.  This saves
-	* me from retyping it.  I'm going to update it to hold it in both vertex
-	* form as well but then we will need to select which form to use.
-	*/
-
-	static float icosahedronVertices[] = {
-		0.0f, 0.0f, 0.0f, 1.0f,
-		0.000000f, -1.000000f, 0.000000f, 1.0f,
-		0.723600f, -0.447215f, 0.525720f, 1.0f,
-		-0.276385f, -0.447215f, 0.850640f, 1.0f,
-		-0.894425f, -0.447215f, 0.000000f, 1.0f,
-		-0.276385f, -0.447215f, -0.850640f, 1.0f,
-		0.723600f, -0.447215f, -0.525720f, 1.0f,
-		0.276385f, 0.447215f, 0.850640f, 1.0f,
-		-0.723600f, 0.447215f, 0.525720f, 1.0f,
-		-0.723600f, 0.447215f, -0.525720f, 1.0f,
-		0.276385f, 0.447215f, -0.850640f, 1.0f,
-		0.894425f, 0.447215f, 0.000000f, 1.0f,
-		0.000000f, 1.000000f, 0.000000f, 1.0f
-	};
-
-
-	static float icosahedronColors[] = {
-		0.0, 0.0, 0.0, 1.0, // Black
-		1.0, 0.0, 0.0, 1.0, // Red
-		1.0, 1.0, 0.0, 1.0, // Yellow
-		0.0, 1.0, 0.0, 1.0, // Green
-		0.0, 0.0, 1.0, 1.0, // Blue
-		1.0, 0.0, 1.0, 1.0, // Magenta
-		1.0, 1.0, 1.0, 1.0, // White
-		0.0, 1.0, 1.0, 1.0,  // Cyan
-		0.0, 0.0, 0.0, 1.0, // Black
-		1.0, 0.0, 0.0, 1.0, // Red
-		1.0, 1.0, 0.0, 1.0, // Yellow
-		0.0, 1.0, 0.0, 1.0, // Green
-		0.0, 0.0, 1.0, 1.0, // Blue
-	};
-
-	static GLuint icosahedronTriangles[] = {
-		1, 2, 3,
-		2, 1, 6,
-		1, 3, 4,
-		1, 4, 5,
-		1, 5, 6,
-		2, 6, 11,
-		3, 2, 7,
-		4, 3, 8,
-		5, 4, 9,
-		6, 5, 10,
-		2, 11, 7,
-		3, 7, 8,
-		4, 8, 9,
-		5, 9, 10,
-		6, 10, 11,
-		7, 11, 12,
-		8, 7, 12,
-		9, 8, 12,
-		10, 9, 12,
-		11, 10, 12
-	};
-
-	float *fileTriangles;
-	/*
-	* Building an array of only vertices that will work with the OpenGL
-	* call for glDrawArray.  In this case we are reading it in using
-	* the ReadOBJ routine.
-	*/
-	fileTriangles = readOBJFile("icodos.obj", nbrTriangles);
-	/*
-	* I'm using wide lines so that they are easier to see on the screen.
-	*/
-	glLineWidth(4.0);
-	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-
-	/*
-	* Create a vertex array for the buffers,
-	* generate an array buffer to copy the
-	* vertex data and the color data into.
-	*/
-
-	glGenVertexArrays(1, vertexBuffers);  // generates ID for VAO
-	glBindVertexArray(vertexBuffers[0]);  // Binds it for use
-	glGenBuffers(1, arrayBuffers);        // Generates ID vor Buffer
-	glBindBuffer(GL_ARRAY_BUFFER, arrayBuffers[0]); // Binds it for use
-	glBufferData(GL_ARRAY_BUFFER,
-		nbrTriangles * 3 * 4 * sizeof(float),
-		fileTriangles,
-		GL_STATIC_DRAW);  // Copies data from fileTriangles into buffer.
-
-	/*
-	*  Use the Books code to load in the shaders.
-	*/
-	ShaderInfo shaders[] = {
-		{ GL_VERTEX_SHADER, "pass.vert" },
-		{ GL_FRAGMENT_SHADER, "pass.frag" },
-		{ GL_NONE, NULL }
-	};
-	GLuint program = LoadShaders(shaders);
-	glUseProgram(program);
-	/*
-	* Set up variables into the shader programs (Note:  We need the
-	* shaders loaded and built into a program BEFORE we do this)
-	*/
-	GLuint vPosition = glGetAttribLocation(program, "vPosition");
-	glEnableVertexAttribArray(vPosition);
-	glVertexAttribPointer(vPosition, 4, GL_FLOAT, GL_FALSE,
-		0, BUFFER_OFFSET(0));
-
-	transformLocation = glGetUniformLocation(program, "transform");
-
-	rotationLocation = glGetUniformLocation(program, "rotation");
-	/*
-	* Enable depth testing to remove hidden surfaces.
-	*/
+void initialize(float *surfaceData, int numberOfPoints) {
+	glClearColor(0.0, 0.0, 0.0, 0.0);
 	glEnable(GL_DEPTH_TEST);
-}
+	GLuint vao, buffer;
+	GLuint transformationLocation;
 
-/*
-* display sets the current transformation and draws the elements
-* in the vertex array.  It does clear the color and depth information
-* before it does this.
-*/
+	static const GLchar *vertexShader = "#version 330 core"
+		""
+		"uniform mat4 transformation;"
+		"in vec4 Position;"
+		""
+		"out vec4 Color;"
+		""
+		"void main() {"
+		"   Color = vec4(1.0, 1.0, 1.0, 1.0);"
+		"	gl_Position = (transformation * Position)/Position.w;"
+		"}"
+		"";
+	static const GLchar *fragmentShader = "#version 330 core"
+		" "
+		"in vec4 Color;"
+		""
+		"void main() {"
+		"   frag_Color = Color; "
+		"}"
+		"";
+	GLuint fragShaderID, vertShaderID, programID;
+	GLint compiledOK;
+
+	programID = glCreateProgram();
+
+	vertShaderID = glCreateShader(GL_VERTEX_SHADER);
+	GLint vertexShaderLength = sizeof(vertexShader);
+	GLint fragmentShaderLength = sizeof(fragmentShader);
+	glShaderSource(vertShaderID, 1, (const GLchar * const *)vertexShader, (const GLint *)&vertexShaderLength);
+	glCompileShader(vertShaderID);
+	glGetShaderiv(vertShaderID, GL_COMPILE_STATUS, &compiledOK);
+	if (!compiledOK) {
+		GLsizei length;
+		glGetShaderiv(vertShaderID, GL_INFO_LOG_LENGTH, &length);
+		GLchar * log = new GLchar[length + 1];
+		glGetShaderInfoLog(vertShaderID, length, &length, log);
+		std::cerr << "Vertex Shader Compilation Failed: " << log << std::endl;
+		delete[] log;
+	}
+	else {
+		glAttachShader(programID, vertShaderID);
+	}
+	fragShaderID = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragShaderID, 1, (const GLchar * const *)fragmentShader, &fragmentShaderLength);
+	glCompileShader(fragShaderID);
+	glGetShaderiv(fragShaderID, GL_COMPILE_STATUS, &compiledOK);
+	if (!compiledOK) {
+		GLsizei length;
+		glGetShaderiv(fragShaderID, GL_INFO_LOG_LENGTH, &length);
+		GLchar * log = new GLchar[length + 1];
+		glGetShaderInfoLog(fragShaderID, length, &length, log);
+		std::cerr << "Fragment Shader Compilation Failed: " << log << std::endl;
+		delete[] log;
+	}
+	else {
+		glAttachShader(programID, fragShaderID);
+	}
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+
+	glGenBuffers(1, &buffer);
+	glBindBuffer(GL_ARRAY_BUFFER, buffer);
+	glBufferData(GL_ARRAY_BUFFER, numberOfPoints, surfaceData, GL_STATIC_DRAW);
+
+}
 
 void display() {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	vmath::mat4 currentMatrix = vmath::scale(0.12f) * vmath::rotate(rotationAngle, 0.0f, 1.0f, 0.0f);
-	glUniformMatrix4fv(transformLocation, 1, GL_TRUE, currentMatrix);
-	glBindVertexArray(vertexBuffers[0]);
-	glBindBuffer(GL_ARRAY_BUFFER, arrayBuffers[0]);
-	glDrawArrays(GL_TRIANGLES, 0, nbrTriangles * 3);
-	glFlush();
+	glDrawArrays(GL_TRIANGLES, 0, 3600 * 3600 * 2);
+	glutSwapBuffers();  // instead of glflush() in double buffering.
+
 }
 
-/*
-* This is the basic main program.  It initializes things and
-* sets up the window.
-*/
-int main(int argCount, char *argValues[]) {
-	glutInit(&argCount, argValues);
+void reshape(int x, int y) {
+}
 
-	// Check for a flag to determine if we are using glDrawElements or 
-	// glDrawArray.  I'm looking for either --elements or --array.
-	// The default is the elements.
-	elements = true;
-	for (int argNbr = 1; argNbr < argCount; argNbr++) {
-		if (argValues[argNbr] == "--elements") {
-			cout << "Drawing using glDrawElements" << endl;
-			elements = true;
-		}
-		else if (argValues[argNbr] == "--array") {
-			elements = false;
-		}
+
+int main(int argCount, char * argValues[]) {
+	float *fileData;
+
+	fileData = readFileData("N36W112.hgt");
+	if (fileData == NULL) {
+		cerr << "Unable to read file or allocate memory -- exitting" << endl;
+		exit(-1);
+	}
+	else {
+		glutInit(&argCount, argValues);
+		glutInitDisplayMode(GLUT_RGBA | GLUT_DEPTH | GLUT_DOUBLE);  // yes -- you want all three
+		glutInitContextVersion(3, 1);
+		glutInitContextProfile(GLUT_CORE_PROFILE);
+		glewInit();
+
+		glutInitWindowSize(600, 600);
+		glutInitWindowPosition(20, 20);
+		glutCreateWindow("Main Window");
+		initialize(fileData, 3600 * 3600 * 2 * 3);
+		glutDisplayFunc(display);
+		glutMainLoop();
+		delete[] fileData;
 	}
 
-	glutInitDisplayMode(GLUT_RGBA | GLUT_DEPTH);
-	glutInitWindowSize(500, 500);
-	glutCreateWindow("Icosahedron Sample");
-
-	glutInitContextVersion(3, 1);
-	glutInitContextProfile(GLUT_CORE_PROFILE);
-	glewInit();
-	init();
-	glutDisplayFunc(display);
-	glutTimerFunc(1000 / 30, timer, 1);
-	glutMainLoop();
-	return 0;
+	return EXIT_SUCCESS;
 }
--- Project1/pass.frag
+++
@@ -1,8 +0,0 @@
-#version 150
-
-out vec4 fColor;
-in vec4 passedColor;
-
-void main() {
-	fColor = passedColor;
-}
